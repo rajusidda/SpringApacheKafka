8c468f0bd325222598572b8fbf0e039c12ca7a16
rajusidda <rajusidda82@gmail.com>
2019-07-03 16:07:46 +0530

    first commit

---
+++ src/test/java/com/apachekafka/apachekafkademo/ApachekafkademoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apachekafka.apachekafkademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApachekafkademoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
